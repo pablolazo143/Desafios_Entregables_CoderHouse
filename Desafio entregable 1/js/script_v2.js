4dec0bb14546ffae0a29cdf7d0e0aa0b081ed9b1
Pablo Lazo <lazopablomiguel@gmail.com>
2022-08-29 22:28:39 -0300

    Proyecto Final Coderhouse - Pablo Lazo

--- Desafio entregable 1/js/script_v2.js
+++ Desafio entregable 1/js/script_v2.js
@@ -24,6 +24,11 @@ let tabla_Puntaje = []; // Tabla de puntajes
 let tablaInicioJuego = document.querySelector('#inicio_juego');
 let repetir_pj = document.querySelector('#repetir_jugador');
 let excusaSeguirJugando = document.querySelector('#excusaSeguir');
+let volverAjugar = document.querySelector('#reiniciar');
+let banderaPuntos;
+agregarJugador.disabled = false;
+iniciarJuego.disabled = false;
+repetir_pj.disabled = false;
 
 // DEFINICIÓN DE LA CLASE JUGADOR
 class jugador{
@@ -32,7 +37,7 @@ class jugador{
         this.puntos = 0;
         this.seguirJugada = true;
         this.salir = false;
-    }
+    };
 };
 
 // FUNCIONES
@@ -52,36 +57,62 @@ function allTrue(jugadores){
 
 function local_storage(clave, valor){
     localStorage.setItem(clave, valor);
-}
+};
 
 function agregarJugadores(){
-    jugadores.push(new jugador(nombreJugador.value));
-    let listaJugadores = document.createElement('li');
-    listaJugadores.innerHTML = nombreJugador.value;
-    tablaJugadores.appendChild(listaJugadores);
-    tablaJugadores.className = 'tablaPuntuacion2'
-    listaJugadores.className = 'tablaPuntuacion';
-    let listaPuntos = document.createElement('li');
-    listaPuntos.innerHTML = `${nombreJugador.value} ${jugadores[index].puntos} puntos`;
-    tablaPuntos.appendChild(listaPuntos);
-    tablaPuntos.className = 'tablaPuntuacion2'
-    listaPuntos.className = 'tablaPuntuacion';
-    nombreJugador.value = '';
-}
+    let bandera = true;
+    // Validar campo de nombre
+    if (nombreJugador.value == ''){
+        nombreJugador.focus();
+        Swal.fire({
+            title: 'Error',
+            text: 'Debe agregar un nombre',
+            icon: 'error',
+        });
+    };
+    if (jugadores.length >= 1){
+        jugadores.forEach(element => {
+            if (element.nombre == nombreJugador.value) {
+                nombreJugador.focus();
+                Swal.fire({
+                    title: 'Error',
+                    text: 'El nombre seleccionado está en uso',
+                    icon: 'error',
+                });
+                bandera = false;
+                nombreJugador.value = '';
+            };
+        });
+    };    
+    if (bandera == true && nombreJugador.value != '') {
+        jugadores.push(new jugador(nombreJugador.value.toUpperCase()));
+        let listaJugadores = document.createElement('li');
+        listaJugadores.innerHTML = nombreJugador.value.toUpperCase();
+        tablaJugadores.appendChild(listaJugadores);
+        tablaJugadores.className = 'tablaPuntuacion2';
+        listaJugadores.className = 'tablaPuntuacion';
+        let listaPuntos = document.createElement('li');
+        listaPuntos.innerHTML = `${nombreJugador.value.toUpperCase()} ${jugadores[index].puntos} puntos`;
+        tablaPuntos.appendChild(listaPuntos);
+        tablaPuntos.className = 'tablaPuntuacion2';
+        listaPuntos.className = 'tablaPuntuacion';
+        nombreJugador.value = '';
+    };
+};
 
 function repetirJugadores(nombreJugador){
     jugadores.push(new jugador(nombreJugador));
     let listaJugadores = document.createElement('li');
     listaJugadores.innerHTML = nombreJugador;
     tablaJugadores.appendChild(listaJugadores);
-    tablaJugadores.className = 'tablaPuntuacion2'
+    tablaJugadores.className = 'tablaPuntuacion2';
     listaJugadores.className = 'tablaPuntuacion';
     let listaPuntos = document.createElement('li');
     listaPuntos.innerHTML = `${nombreJugador} ${jugadores[index].puntos} puntos`;
     tablaPuntos.appendChild(listaPuntos);
-    tablaPuntos.className = 'tablaPuntuacion2'
+    tablaPuntos.className = 'tablaPuntuacion2';
     listaPuntos.className = 'tablaPuntuacion';
-}
+};
 
 function imprimirTabla(){
     tablaDOM.innerHTML = null;
@@ -91,59 +122,97 @@ function imprimirTabla(){
     tabla_Puntaje.sort(function (a, b) {
         if (a.puntos < b.puntos) {
         return 1;
-        }
+        };
         if (a.puntos > b.puntos) {
         return -1;
-        }
+        };
         // a.puntos = b.puntos
         return 0;
     });
+    let contador = 1;
     for (const elemento of tabla_Puntaje){
         let lista = document.createElement('li');
-        lista.innerHTML = `Jugador: ${elemento.nombre} Puntos: ${elemento.puntos}`;
+        if (contador == 1){
+            lista.innerHTML = `🥇 ${elemento.nombre}  Puntos: ${elemento.puntos}`;
+        }else if (contador == 2){
+            lista.innerHTML = `🥈 ${elemento.nombre} Puntos: ${elemento.puntos}`;
+        }else if (contador == 3){
+            lista.innerHTML = `🥉 ${elemento.nombre} Puntos: ${elemento.puntos}`;
+        }else{
+            lista.innerHTML = `😭 ${elemento.nombre} Puntos: ${elemento.puntos}`;
+        };
         lista.className = 'tablaPuntuacion';
         tablaDOM.appendChild(lista);
+        contador++;
     };
-}
+
+    banderaPuntos = tabla_Puntaje[0]?.puntos || null;
+    banderaPuntos ? banderaPuntos = true : banderaPuntos = false ; 
+    if (banderaPuntos == true && tabla_Puntaje[0].puntos >= 50){
+        Swal.fire({
+            position: 'center',
+            icon: 'info',
+            title: `Felicidades ${tabla_Puntaje[0].nombre} has ganado! 🏆`,
+            imageUrl: '/img/Dados_good_luck.jpg',
+            imageWidth: 400,
+            imageHeight: 200,
+            imageAlt: 'Custom image',
+            showConfirmButton: false,
+            timer: 4000
+          });
+    }else{
+        Swal.fire({
+            position: 'center',
+            icon: 'info',
+            title: `Juego terminado`,
+            showConfirmButton: false,
+            timer: 1500
+          });
+    }
+};
 
 function agregarPuntaje(index){
     tablaPuntos.children[index+1].textContent = `${jugadores[index].nombre} ${jugadores[index].puntos} puntos`;
-}
+};
 
 // EVENTOS
 // Evento para mostrar instrucciones de juego
 instrucciones.addEventListener('click', () => {
-    // alert('Se decide que jugador debe empezar. El jugador empieza tira los dados. Si saca 2, 3, 4, 5, 6 acumula puntos y puede decidir si sigue jugando o pasa su turno. Si saca un 1, no gana puntuación e inmediatamente pasa el dado al siguiente jugador, perdiendo todos los puntos acumulados en ese turno. GANA EL JUGADOR QUE ALCANZA 50 PUNTOS')
-    Swal.fire({
-        title: 'Instrucciones',
-        text: 'Se decide que jugador debe empezar. El jugador empieza tira los dados. Si saca 2, 3, 4, 5, 6 acumula puntos y puede decidir si sigue jugando o pasa su turno. Si saca un 1, no gana puntuación e inmediatamente pasa el dado al siguiente jugador, perdiendo todos los puntos acumulados en ese turno. GANA EL JUGADOR QUE ALCANZA 50 PUNTOS. Puede generar una EXCUSA para seguir jugando.',
-        icon: 'info',
-        // confirmButtonText: ''
-    })
-})
+    const archivoJSON ='/info.json';
+    fetch(archivoJSON)
+    .then(resp => resp.json())
+    .then(data => {
+        const infoText = data[0].informacion;
+        Swal.fire({
+            title: 'Instrucciones',
+            text: `${infoText}`,
+            icon: 'info',
+        });
+    });
+});
 
 // Evento para agregar jugador
 agregarJugador.addEventListener('click', agregarJugadores);
 
 // Evento para pasar el turno
 pasar_juego.addEventListener('click', ()=>{
-    console.log('click en pasar turno')
+    console.log('click en pasar turno');
     if (iniciar_juego == true){
         jugadores[index].seguirJugada = false;
-        // alert(`Cambio de turno, el jugador ${jugadores[index].nombre} pasó el turno`);
-        Toastify({
-            text: `Cambio de turno, el jugador ${jugadores[index].nombre} pasó el turno`,
-            duration: 2000,
-            gravity: 'bottom',
-            position: 'right'
-        }).showToast();
+        Swal.fire({
+            position: 'center',
+            icon: 'info',
+            title: `El jugador ${jugadores[index].nombre} pasó el turno`,
+            showConfirmButton: false,
+            timer: 1500
+          });
         puntosInicio[index] = jugadores[index].puntos;
         index++;
         if(index >= jugadores.length){
             index = 0;
-        }
+        };
         allTrue(jugadores);
-    } 
+    };
 });
 
 // Evento para terminar juego
@@ -151,6 +220,7 @@ terminar_juego.addEventListener('click', (e)=>{
     e.preventDefault;
     console.log('click en salir');
     iniciar_juego = false;
+    volverAjugar.classList.add('white');
     imprimirTabla();
 });
 
@@ -158,12 +228,33 @@ terminar_juego.addEventListener('click', (e)=>{
 iniciarJuego.addEventListener('click', (e)=>{
     e.preventDefault;
     console.log('click en iniciar');
-    local_storage('nombreJugadores', JSON.stringify(jugadores));
-    iniciar_juego = true;
-    for(i = 0; i < jugadores.length; i++){
-        puntosInicio[i] = jugadores[i].puntos;
-    }
-    tablaInicioJuego.className = 'visible';
+    if(jugadores.length >= 2){
+        local_storage('nombreJugadores', JSON.stringify(jugadores));
+        iniciar_juego = true;
+        for(i = 0; i < jugadores.length; i++){
+            puntosInicio[i] = jugadores[i].puntos;
+        };
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Start!',
+            showConfirmButton: false,
+            timer: 1500
+          });
+        tablaInicioJuego.className = 'visible';
+        agregarJugador.disabled = true;
+        iniciarJuego.disabled = true;
+        repetir_pj.disabled = true;
+    }else{
+        nombreJugador.focus();
+        Swal.fire({
+            position: 'center',
+            icon: 'warning',
+            title: `Debe haber al menos 2 jugadores`,
+            showConfirmButton: false,
+            timer: 2000
+          });
+    };
 });
 
 // Evento tirar dado
@@ -184,28 +275,29 @@ tirarDado.addEventListener('click', function jugada(){
             index++;
             if(index >= jugadores.length){
                 index = 0;
-            }
-            // alert(`Cambio de turno, obtuviste un 1`);
-            Toastify({
-                text: `Cambio de turno, obtuviste un 1`,
-                duration: 2000,
-                gravity: 'bottom',
-                position: 'right'
-            }).showToast();
+            };
+            Swal.fire({
+                position: 'center',
+                icon: 'info',
+                title: `Cambio de turno, obtuviste un 1`,
+                showConfirmButton: false,
+                timer: 2000
+              });
         }
         if(index < jugadores.length && jugadores[index].puntos >= puntosFinal){
             puntosInicio[index] = jugadores[index].puntos;
             imprimirTabla();
-        } 
-    }
+            volverAjugar.classList.add('white');
+        }; 
+    };
 });
 
 // Evento repetir Personajes
 let repetir_personaje = [];
 let repetir_switch = false;
+
 repetir_pj.addEventListener('click', (e) => {
     e.preventDefault();
-
     // Extraer de localStorage
     repetir_personaje = JSON.parse(localStorage.getItem("nombreJugadores")) || null;
     repetir_personaje ? repetir_switch = true : repetir_switch = false ; 
@@ -215,34 +307,26 @@ repetir_pj.addEventListener('click', (e) => {
         });
         for(i = 0; i < jugadores.length; i++){
             puntosInicio[i] = jugadores[i].puntos;
-        }
+        };
         iniciar_juego = true;
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Start!',
+            showConfirmButton: false,
+            timer: 1500
+          });
         tablaInicioJuego.className = 'visible';
     }else{
-        // alert('no hay pj en base de datos');
         Swal.fire({
             title: 'Warning',
             text: 'No hay jugadores guardados en la base de datos',
             icon: 'error',
-            // confirmButtonText: ''
-        })
-    }
-} )
-
-
-// Evento pedir excusa para seguir jugando
-excusaSeguirJugando.addEventListener('click',(e)=>{
-    e.preventDefault();
-    const URL ='https://excuser.herokuapp.com/v1/excuse/party';
-    fetch(URL)
-    .then(resp => resp.json())
-    .then(data => {
-        const excusaTexto = data[0].excuse;
-        Swal.fire({
-            title: 'Excusa',
-            text: `${excusaTexto}`,
-            icon: 'info',
         });
-    });
+    };
+    agregarJugador.disabled = true;
+    iniciarJuego.disabled = true;
+    repetir_pj.disabled = true;
+});
 
-})
+
